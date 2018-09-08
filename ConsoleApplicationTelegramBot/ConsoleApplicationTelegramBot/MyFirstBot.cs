eddd8123c5a9cd59b9454737f991ab93adefb3b3
Liorby <liorby530@gmail.com>
2018-09-08 19:45:50 +0300

    first

---
+++ ConsoleApplicationTelegramBot/ConsoleApplicationTelegramBot/MyFirstBot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace ConsoleApplicationTelegramBot
+{
+    class MyFirstBot
+    {
+        private static readonly TelegramBotClient m_Bot = 
+            new TelegramBotClient("647698451:AAEjsJ-U_otsl80fT5UyXEENPQ7sEqQuErw");
+
+        public void ReceivingMessage()
+        {
+            m_Bot.OnMessage += M_Bot_OnMessage;
+            m_Bot.OnMessageEdited += M_Bot_OnMessage;
+            m_Bot.StartReceiving();
+            Console.ReadLine();
+            m_Bot.StopReceiving();
+            
+        }
+
+        private void M_Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+           if(e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                if (e.Message.Text == "How r u?")
+                {
+                    m_Bot.SendTextMessageAsync(e.Message.Chat.Id, "I'm goood!" + e.Message.Chat.Username);
+                }
+                else
+                {
+                    m_Bot.SendTextMessageAsync(e.Message.Chat.Id, "I dont know");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApplicationTelegramBot/ConsoleApplicationTelegramBot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Telegram.Bot;
+namespace ConsoleApplicationTelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyFirstBot bot = new MyFirstBot();
+            bot.ReceivingMessage();
+        }
+    }
+}
